fafde933b4f0e5dfee588c36cb42e493f145db8e
Thomas Liu <liuxk99@gmail.com>
2019-06-11 15:01:17 +0800

    <feature>: Context-registered receiver

--- app/src/main/java/com/sj/app/SjActivity.java
+++ app/src/main/java/com/sj/app/SjActivity.java
@@ -1,13 +1,51 @@
 package com.sj.app;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
+import com.sj.utils.SjLog;
+import com.sj.utils.SjLogGen;
+
 public class SjActivity extends Activity {
+    final String TAG = SjActivity.class.getSimpleName();
+    SjLogGen mLogGen = new SjLogGen(TAG);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        SjLog sjLog = mLogGen.build("onCreate(" + savedInstanceState + ")");
+        sjLog.in();
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+        sjLog.out();
+    }
+
+    final IntentFilter mFilter = new IntentFilter("sj.intent.action.ACTION_DYNAMIC");
+    final BroadcastReceiver mReceiver = new SjDynamicReceiver();
+
+    @Override
+    protected void onPause() {
+        SjLog sjLog = mLogGen.build("onPause()");
+        sjLog.in();
+        {
+            unregisterReceiver(mReceiver);
+            super.onPause();
+        }
+        sjLog.out();
+    }
+
+    @Override
+    protected void onResume() {
+        SjLog sjLog = mLogGen.build("onResume()");
+        sjLog.in();
+        {
+            super.onResume();
+
+            registerReceiver(mReceiver, mFilter);
+        }
+        sjLog.out();
     }
 }

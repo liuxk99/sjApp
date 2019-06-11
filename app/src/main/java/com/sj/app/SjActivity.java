2d2e97e8d9f9a95978158d05b8bd029388c4731a
Thomas Liu <liuxk99@gmail.com>
2019-06-11 13:48:01 +0800

    <feature>: add SjActivity

---
+++ app/src/main/java/com/sj/app/SjActivity.java
@@ -0,0 +1,13 @@
+package com.sj.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SjActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
